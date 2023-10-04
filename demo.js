992bd3c40f448e043f55c850ec1b0375546095a1
SOWMYASAI19 <124090046+SOWMYASAI19@users.noreply.github.com>
2023-10-04 15:58:41 -0500

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,53 @@
+let data=[
+    {id:1,name:'sowmi',courses:['math','bio','chemistry'],cgpa:3.6},
+    {id:2,name:'sravya',courses:['math','bio','Physics'],cgpa:3.6},
+    {id:3,name:'Nikki',courses:['chemistry','bio','geometry'],cgpa:3.6},
+    {id:4,name:'noor',courses:['social','bio','chemistry'],cgpa:3.6},
+
+]
+
+//length,push,findIndex,forEach,map,filter,flatmap
+let st={id:5,name:'Pavani',courses:[],cgpa:0};
+console.log('no of students',data.length);
+data.push(st)
+
+//length is used the number of elements in the array
+console.log('no of students',data.length);
+console.log(data)
+console.log('no of students', data.length);
+
+data.forEach(x=>console.log(x.name,'--->',x.cgpa));
+
+//data.forEach(x=>console.log(x.courses))
+
+//It is used to add one or more elements to the end of an array .
+ data.forEach(x=>x.courses.push('alg'))
+
+console.log(data)
+let unq_courses=[]
+//FindIndex is used for the index of the array.
+//The flatMap method is used to extract all of the classes in the data array from each student and generate a flattened array of unique courses.
+let all_courses=data.flatMap(x=>x.courses)
+//forEach method is used to loop through the data array and display the name and CGPA of each student to the console.
+all_courses.forEach(x=>{
+    if(!unq_courses.includes(x)){
+        unq_courses.push(x)
+    }
+})
+console.log(unq_courses);
+//console.log(all_courses)
+
+//let search='alg'
+unq_courses.forEach(y=>{
+
+    //The filter() method is used to filter and return a new array of objects with a particular property value.
+    console.log(y,data.filter(x=>x.courses.includes(y)).length)
+})
+//console.log(data.filter(x=>x.courses.includes(search)).length)
+
+//The map() function is used to extract the cgpa property of each object in the data array and create a new array having only these cgpa values.
+let all_cgpa=data.map(x=>x.cgpa)
+console.log(all_cgpa)
+//console.log(data.map(x=>x.courses).flatMap(x=>x))
+//console.log(data.map(x=>x.courses))
+//console.log(data.flatMap(x=>x.courses))
